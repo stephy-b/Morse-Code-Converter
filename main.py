2d2a80d756ddff189447c92298618b5ab49bb16d
s-biagini <109245563+s-biagini@users.noreply.github.com>
2022-11-20 13:58:28 -0800

    Punctuation & Function Start
    Added lists for punctuation conversion.  Started convert_to_morse function.

--- main.py
+++ main.py
@@ -4,6 +4,30 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
+punctuation = ['.', ',', '?', '!', "'"]
+
 morse_alphabet = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-.--', '--..']
 
 morse_numbers = ['.---', '..---', '...--', '....-', '.....', '-....', '--...', '---..', '----.', '-----']
+
+morse_punctuation = ['.-.-.-', '--..--', '..--..', '-.-.--', '.----.']
+
+
+def convert_to_morse(starting_text):
+    '''Converts any String to Morse Code.  Regarding punctuation, only periods, commas, question marks, exclamation points, and apostrophes are converted if used.'''
+    
+    final_text = ""
+    
+    for char in starting_text:
+        if char in alphabet:
+            new_char = alphabet.index(char)
+            morse_char = morse_alphabet[new_char]
+            final_text += morse_char
+            
+        elif char in starting_text == ' ':
+            morse_char = '.......'
+            final_text += morse_char
+            
+        elif char in starting_text in punctuation:
+            new_punc = punctuation.index(char)
+            morse_punc = morse_punctuation[morse_punc]
