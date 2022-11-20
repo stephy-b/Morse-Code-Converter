e150031b661ea37eab082dfc055e3eb24d74c3fd
s-biagini <109245563+s-biagini@users.noreply.github.com>
2022-11-20 14:27:08 -0800

    Finished Function, Added While Loop
    Convert function now returns the final text.  Added a while loop to run the program.  **Bug Note: converting a space into 7 dots is not yet working**

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from art import logo
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 punctuation = ['.', ',', '?', '!', "'"]
 
-morse_alphabet = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-.--', '--..']
+morse_alphabet = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
 
 morse_numbers = ['.---', '..---', '...--', '....-', '.....', '-....', '--...', '---..', '----.', '-----']
 
@@ -22,12 +22,39 @@ def convert_to_morse(starting_text):
         if char in alphabet:
             new_char = alphabet.index(char)
             morse_char = morse_alphabet[new_char]
-            final_text += morse_char
+            final_text += morse_char + ' '
+            
+        elif char in numbers:
+            new_num = numbers.index(char)
+            morse_num = morse_numbers[new_num]
+            final_text += morse_num + ' '
+                        
+        elif char in punctuation:
+            new_punc = punctuation.index(char)
+            morse_punc = morse_punctuation[new_punc]
+            final_text += morse_punc + ' '
             
         elif char in starting_text == ' ':
             morse_char = '.......'
-            final_text += morse_char
+            final_text += morse_char + ' '
             
-        elif char in starting_text in punctuation:
-            new_punc = punctuation.index(char)
-            morse_punc = morse_punctuation[morse_punc]
+        else:
+            final_text += char
+            
+    return final_text
+            
+
+print(logo)
+
+game_on = True
+while game_on:
+    message_to_convert = input('Type a message to convert into Morse Code:\n').lower()
+    
+    converted_message = convert_to_morse(message_to_convert)
+    print(converted_message)
+    
+    restart = input(f'Do you want to convert another message?  Type "yes" or "no".\n')
+    
+    if restart == "no":
+        print('Ok, goodbye.')
+        game_on = False
